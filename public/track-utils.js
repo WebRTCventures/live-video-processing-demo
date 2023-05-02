ebb997a21cf22837aecd45d1720bf45b5100699e
Hector Zelaya <inge.zelaya@gmail.com>
2023-05-02 15:26:11 -0600

    added ability to change effect

--- public/track-utils.js
+++ public/track-utils.js
@@ -11,9 +11,15 @@ function createProcessedTrack({ track, transform }) {
   return trackGenerator;
 }
 
+function cleanStream() {
+  return function transform(frame, controller) {
+    controller.enqueue(frame);
+  }
+}
+
 function showText({
   text,
-  track,
+  txtInitialX,
   txtColor = 'white',
   txtFontSize = '48px',
   txtFont = 'serif',
@@ -22,23 +28,28 @@ function showText({
   bgPadding = 10,
   position = 'top'
 }) {
-  const { width, height } = track.getSettings();
-  const canvas = new OffscreenCanvas(width, height);
+  const canvas = new OffscreenCanvas(1, 1);
   const ctx = canvas.getContext('2d');
-
   const intTxtFontSize = parseInt(txtFontSize);
-  const bgHeight = intTxtFontSize + bgPadding;
-  const bgPositionY = position === 'bottom' 
-    ? height - (intTxtFontSize + bgPadding + 5) 
-    : 5;
-  const txtPositionY = position === 'bottom' 
-    ? height - (intTxtFontSize + Math.floor(bgPadding/2) + 5) 
-    : 5 + intTxtFontSize;
 
-  let x = width + 100;
-  function transform(videoFrame, controller) {
+  let x = txtInitialX;
+
+  return function transform(frame, controller) {
+    const width = frame.displayWidth;
+    const height = frame.displayHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    const bgHeight = intTxtFontSize + bgPadding;
+    const bgPositionY = position === 'bottom'
+      ? height - (intTxtFontSize + bgPadding + 5)
+      : 5;
+    const txtPositionY = position === 'bottom'
+      ? height - (intTxtFontSize + Math.floor(bgPadding / 2) + 5)
+      : 5 + intTxtFontSize;
+
     ctx.clearRect(0, 0, width, height);
-    ctx.drawImage(videoFrame, 0, 0, width, height);
+    ctx.drawImage(frame, 0, 0, width, height);
     ctx.font = txtFontSize + ' ' + txtFont;
     ctx.fillStyle = bgColor;
     ctx.fillRect(0, bgPositionY, width, bgHeight)
@@ -47,13 +58,12 @@ function showText({
 
     x -= textSpeed;
     if (x <= (0 - 100 - text.length * 20)) {
-      x = width + 100
+      x = width
     }
-    const newFrame = new VideoFrame(canvas, { timestamp: videoFrame.timestamp });
 
-    videoFrame.close();
+    const newFrame = new VideoFrame(canvas, { timestamp: frame.timestamp });
+
+    frame.close();
     controller.enqueue(newFrame);
   }
-
-  return createProcessedTrack({ track, transform });
 }
